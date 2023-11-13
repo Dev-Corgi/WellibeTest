d48d2c9f2915a2212c97789503ea38d2bfe1df0a
Dev-Corgi <bora5242@gmail.com>
2023-11-13 16:19:05 +0900

    error fixed

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ import AppLoading from "expo-app-loading";
 const Stack = createNativeStackNavigator();
 export const navigationRef = createNavigationContainerRef();
 
-const update = "h111";
+const update = "h11";
 
 function App() {
 
