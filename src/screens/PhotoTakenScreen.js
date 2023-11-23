7c749527679f3f96844810ebdea5db036f637d57
Dev-Corgi <bora5242@gmail.com>
2023-11-23 21:53:58 +0900

    camera fix

--- src/screens/PhotoTakenScreen.js
+++ src/screens/PhotoTakenScreen.js
@@ -7,6 +7,9 @@ import { AutoFocus, Camera, CameraType } from 'expo-camera';
 
 const PhotoTakenScreen = () => {
 
+
+  const cameraRef = useRef(null);
+
   const takePictureHandler = async () => { 
     // cameraRef가 없으면 해당 함수가 실행되지 않게 가드
     if (!cameraRef.current) return;
@@ -23,17 +26,88 @@ const PhotoTakenScreen = () => {
       });
   };
 
-  const cameraRef = useRef(null);
+  ////
+  //  camera permissions
+  const [hasCameraPermission, setHasCameraPermission] = useState(null);
+  const [camera, setCamera] = useState(null);
+
+  // Screen Ratio and image padding
+  const [imagePadding, setImagePadding] = useState(0);
+  const [ratio, setRatio] = useState('4:3');  // default is 4:3
+  const { height, width } = Dimensions.get('window');
+  const screenRatio = height / width;
+  const [isRatioSet, setIsRatioSet] =  useState(false);
+
+  // on screen  load, ask for permission to use the camera
+  useEffect(() => {
+    async function getCameraStatus() {
+      const { status } = await Camera.requestPermissionsAsync();
+      setHasCameraPermission(status == 'granted');
+    }
+    getCameraStatus();
+  }, []);
+
+  // set the camera ratio and padding.
+  // this code assumes a portrait mode screen
+  const prepareRatio = async () => {
+    let desiredRatio = '4:3';  // Start with the system default
+    // This issue only affects Android
+    if (Platform.OS === 'android') {
+      const ratios = await camera.getSupportedRatiosAsync();
+
+      // Calculate the width/height of each of the supported camera ratios
+      // These width/height are measured in landscape mode
+      // find the ratio that is closest to the screen ratio without going over
+      let distances = {};
+      let realRatios = {};
+      let minDistance = null;
+      for (const ratio of ratios) {
+        const parts = ratio.split(':');
+        const realRatio = parseInt(parts[0]) / parseInt(parts[1]);
+        realRatios[ratio] = realRatio;
+        // ratio can't be taller than screen, so we don't want an abs()
+        const distance = screenRatio - realRatio; 
+        distances[ratio] = realRatio;
+        if (minDistance == null) {
+          minDistance = ratio;
+        } else {
+          if (distance >= 0 && distance < distances[minDistance]) {
+            minDistance = ratio;
+          }
+        }
+      }
+      // set the best match
+      desiredRatio = minDistance;
+      //  calculate the difference between the camera width and the screen height
+      const remainder = Math.floor(
+        (height - realRatios[desiredRatio] * width) / 2
+      );
+      // set the preview padding and preview ratio
+      setImagePadding(remainder);
+      setRatio(desiredRatio);
+      // Set a flag so we don't do this 
+      // calculation each time the screen refreshes
+      setIsRatioSet(true);
+    }
+  };
+
+  // the camera must be loaded in order to access the supported ratios
+  const setCameraReady = async() => {
+    if (!isRatioSet) {
+      await prepareRatio();
+    }
+  };
 
   return (
     <View style = {styles.view}>
       <Camera
-        ref={cameraRef}
-        type={CameraType.front}
-        zoom={0}
-        autoFocus={AutoFocus.on}
-        style = {styles.camera}
-        ratio='16:9'
+      onCameraReady={setCameraReady}
+      ref={cameraRef}
+      ratio = {ratio}
+      type={CameraType.front}
+      zoom={0}
+      autoFocus={AutoFocus.on}
+      style = {styles.camera}
       />
      <View style={styles.guideGridFrame}>
       <View style={styles.guideGrid}>
